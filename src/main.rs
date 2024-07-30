136f0fe3c4cb7d2ce016695e78dead6553fc759c
Alligator-52 <ayushankitpatnaik99@gmail.com>
2024-07-30 23:35:08 +0530

    just some basic code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use rand::Rng;
+fn main() 
+{
+    println!("welcome to my first rust mini project!");
+
+    let secret_num:u16 = rand::thread_rng().gen_range(0..=420);
+    //let mut is_win:bool = false;
+    println!("\n Enter your guess:");
+
+    let mut guess:String = String::new();
+    io::stdin().read_line(&mut guess).expect("no line read");
+    // if(integer == secret_num){
+    //     println!("Do something");
+    // }
+    println!("You guessed {guess}");
 }
