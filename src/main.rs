9e0c9620d4eef0a9b94384f4e08e8059785bdae7
Alligator-52 <ayushankitpatnaik99@gmail.com>
2024-07-31 08:08:44 +0530

    Done with guessing game! On to next chapter!

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
-use std::io;
 use rand::Rng;
-fn main() 
-{
+use std::cmp::Ordering;
+use std::io;
+fn main() {
     println!("welcome to my first rust mini project!");
 
-    let secret_num:u16 = rand::thread_rng().gen_range(0..=420);
+    let secret_num: u32 = rand::thread_rng().gen_range(0..=420);
     //let mut is_win:bool = false;
-    println!("\n Enter your guess:");
+    loop // loop allows to keep this code running within loop
+    {
+        println!("\n Enter your guess:");
 
-    let mut guess:String = String::new();
-    io::stdin().read_line(&mut guess).expect("no line read");
-    // if(integer == secret_num){
-    //     println!("Do something");
-    // }
-    println!("You guessed {guess}");
+        let mut guess: String = String::new();
+        io::stdin().read_line(&mut guess).expect("No line read!");
+
+        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+
+        //if else implement
+
+        // if guess == secret_num //.into converts the type from u16 to u32
+        // {
+        //     println!("you win!");
+        // }
+        // else
+        // {
+        //     print!("you lose! :( ");
+        // }
+
+        match guess.cmp(&secret_num) {
+            Ordering::Equal => {
+                print!("you win! ");
+                println!("You guessed {guess}, the number was {secret_num}");
+                break;
+            }
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
+
+    //println!("You guessed {guess}, the number was {secret_num}");
 }
